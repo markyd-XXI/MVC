8f3ea01ad99a25983c2af793993c9a97a0620ea2
Mark Delgado <sandmdelgados@gmail.com>
2023-02-14 12:14:20 -0600

    Adding more initial files.

---
+++ src/main/java/com/example/OOPMVC/OopmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.OOPMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OOPMVC/OopmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OOPMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
